59ebadb47c1b7fe90c7d1500b1a24eb27d228e85
SupremeNoble <Syed9098@gmail.com>
2023-04-11 15:01:30 +0800

    login and register function update

--- noble.js
+++ noble.js
@@ -12,18 +12,77 @@ app.get('/bruh', (req, res) => {
   )
 })
 
+app.use(express.json())
+app.post('/register', (req, res) => {
+  console.log(req.body)
 
-app.get('/register', (req, res) => {
-  res.send('Account Created')
+  let result = register(req.body.username, req.body.password,req.body.name,req.body.email)
+
+  res.send(result)
 })
 
 app.use(express.json())
 app.post('/login', (req, res) => {
   console.log(req.body)
 
-  res.send('Login')
+  let result = login(req.body.username, req.body.password)
+
+  res.send(result)
+})
+
+app.get('/shxtou', (req, res) => {
+  res.sendFile(__dirname + '/shxtouMagnify.png');
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+let dbUsers = [
+  {
+      username: "Noble",
+      password: "abc123",
+      name: "Ahmad",
+      email: "supremenoble@gmail.com"
+  },
+  {
+      username: "Myto",
+      password: "seliparterbang",
+      name: "Fadzlan",
+      email: "mytosuper@gmail.com"
+  },
+  {
+      username: "Zyntex",
+      password: "miloaispanas0",
+      name: "Zaid",
+      email: "presidentjaksis12@gmail.com"
+  },
+  {
+      username: "Scufie",
+      password: "mobiletaklegend05",
+      name: "Hamdi",
+      email: "datukserihamdi@gmail.com"
+  }
+]
+
+function login(reqUsername,reqPassword) {
+  let matchUser = dbUsers.find(
+      user => user.username == reqUsername
+  )
+  //console.log(matchUser)
+  if(!matchUser) return "User not found!"
+  if(matchUser.password == reqPassword) {
+      return matchUser
+  } else {
+      return "invalid password!"
+  }
+}
+
+function register(reqUsername,reqPassword,reqName,reqEmail){
+  dbUsers.push({
+      username: reqUsername,
+      password: reqPassword,
+      name: reqName,
+      email: reqEmail
+  })
+}
